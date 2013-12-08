bb6ee9f34e94eaf2dabb6bfe028ecf3ae70300d8
C. Cho <jamieclcho@gmail.com>
2013-12-09 01:21:46 +0900

    first commit

--- AndroidTest02/src/clcho/androidtest02/FullscreenActivity.java
+++ AndroidTest02/src/clcho/androidtest02/FullscreenActivity.java
@@ -14,6 +14,8 @@ import android.view.View;
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  * 
+ * comment added by clcho
+ * 
  * @see SystemUiHider
  */
 public class FullscreenActivity extends Activity {
